3a36c34ff972ec7fb8e1ed6cc663431a81f42a30
iamfiro <appturbo102@gmail.com>
2025-01-01 12:32:38 +0900

    feat(workspace): add scope rules to commitlint configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,15 @@
 module.exports = {
 	extends: ['@commitlint/config-conventional'],
 		rules: {
-			"type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "test", "chore", "revert"]],
+			"type-enum": [2, "always", ["feat", "fix", "docs", "style", "refactor", "test", "chore", "revert", "docs"]],
+      "scope-case": [2, "always", "lower-case"],
+      "scope-empty": [2, "never"],
+      "scope-enum": [2, "always", [
+        "react",
+        "native",
+        "hook",
+        "lib",
+        "workspace"
+      ]]
 		},
 };
