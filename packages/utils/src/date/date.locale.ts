da24f49110bdb3be7686c991a3685cfea07503c6
FIRO <72495729+iamfiro@users.noreply.github.com>
2025-01-06 10:05:25 +0900

    refactor(utils): enhance getDateName function to support dynamic language options and remove unused constant

--- packages/utils/src/date/date.locale.ts
+++ packages/utils/src/date/date.locale.ts
@@ -1,5 +1,3 @@
-export const supportedLang = ['ko', 'en'] as const;
-
 export const dateName: {[key in string]: ReadonlyArray<string>} = {
     ko: ['일', '월', '화', '수', '목', '금', '토'],
     en: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
--- packages/utils/src/date/getDateName.ts
+++ packages/utils/src/date/getDateName.ts
@@ -6,9 +6,14 @@ import { dateName } from "./date.locale"
  * @param options lang: 'ko' | 'en'
  * @returns {string} 해당 요일의 이름
  */
-export function getDateName(date: Date, options?: { lang: 'ko' | 'en' }): string {
+export function getDateName(date: Date, options?: { lang: keyof typeof dateName }): string {
     // lang 옵션을 체크하고 없으면 기본값을 설정합니다.
-    if(typeof options === 'undefined') { options = { lang: 'ko' } };
+    options = options || { lang: 'ko' }; // 기본값은 ko
+
+    // 지원하지 않는 언어 체크
+    if(!dateName[options.lang]) {
+        throw new Error('지원하지 않는 언어입니다.')
+    };
     
     return dateName[options.lang][date.getDay()];
 }
